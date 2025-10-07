4a31b14690f25e8bf885e7d6fd3d3504cc74ea9f
KartikG2 <kartikgangavati2004@gmail.com>
2025-10-07 19:09:56 +0530

    added a cocktail

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,11 +1,14 @@
 import { useGSAP } from '@gsap/react'
 import gsap from 'gsap'
 import { SplitText } from 'gsap/all'
-import React from 'react'
-
+import React, { useRef } from 'react'
+import {useMediaQuery} from 'react-responsive';
 
 
 const Hero = () => {
+    const videoRef = useRef();
+
+    const isMobile = useMediaQuery({maxWidth : 767})
 
     useGSAP(()=>{
         const heroSplit = new SplitText('.title',{type : 'chars , words'});
@@ -39,10 +42,30 @@ const Hero = () => {
             }
         }).to('.right-leaf' , {y : -200},0)
         .to('.left-leaf' , {y : 200},0)
+
+        const startValue = isMobile ? 'top 50%' : 'center 60%';
+        const endValue = isMobile ? '120% top'  : 'bottom top';
+
+
+        const tl = gsap.timeline({
+            scrollTrigger : {
+                trigger : 'video',
+                start : startValue,
+                end:endValue,
+                scrub : true,
+                pin : true
+            }
+        })
+
+        videoRef.current.onloadedmetadata = ()=>{
+            tl.to(videoRef.current,{
+                currentTime : videoRef.current.duration
+            })
+        }
     },[])
     return (
         <>
-            <section id='hero' className="noisy">
+            <section id='hero' className="">
                 <h1 className='title'>COCKTAIL</h1>
                 <img src="/images/hero-left-leaf.png" alt="left-leaf"  className='left-leaf'/>
                 <img src="/images/hero-right-leaf.png" alt="right-leaf" className='right-leaf'/>
@@ -66,6 +89,9 @@ const Hero = () => {
                 </div>
 
             </section>
+            <div className="video absolute inset-0">
+                <video src='/videos/output.mp4' ref={videoRef} muted playsInline preload='auto'/>
+            </div>
 
         </>
     )
