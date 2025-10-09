21faf802e610e082d281a1051f298ef5b18a1fef
KartikG2 <kartikgangavati2004@gmail.com>
2025-10-09 22:40:47 +0530

    final commit

--- src/app.jsx
+++ src/app.jsx
@@ -4,7 +4,7 @@ import { ScrollTrigger, SplitText } from 'gsap/all';
 
 import Navbar from './components/Navbar';
 import Hero from './components/Hero';
-import Cocktails from './components/cocktails';
+import Cocktails from './components/Cocktails';
 import About from './components/About';
 import Art from './components/Art';
 import Menu from './components/Menu';
