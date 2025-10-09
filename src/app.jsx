9636353007d6e3f5ebc61fa051f1b99d8e8a624c
KartikG2 <kartikgangavati2004@gmail.com>
2025-10-09 12:34:58 +0530

    added one more  section

--- src/app.jsx
+++ src/app.jsx
@@ -6,6 +6,7 @@ import Navbar from './components/Navbar';
 import Hero from './components/Hero';
 import Cocktails from './components/cocktails';
 import About from './components/About';
+import Art from './components/Art';
 
 
 
@@ -19,6 +20,7 @@ const App = () => {
     <Hero/>
     <Cocktails/>
     <About/>
+    <Art/>
   </main>
   );
 };
